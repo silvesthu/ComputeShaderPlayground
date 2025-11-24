73844152a0cf59f8f125ee0ad292a91aa5a14980
Ken Hu <silvesthu@gmail.com>
2025-11-25 03:48:31 +0900

    Update D3D12 to 1.618.3, dxc to v1.8.2505.1

--- ComputeShaderPlayground.cpp
+++ ComputeShaderPlayground.cpp
@@ -8,7 +8,7 @@
 #include <wrl.h>
 using namespace Microsoft::WRL;
 
-extern "C" { __declspec(dllexport) extern const UINT			D3D12SDKVersion = 614; }
+extern "C" { __declspec(dllexport) extern const UINT			D3D12SDKVersion = 618; }
 extern "C" { __declspec(dllexport) extern const char8_t*		D3D12SDKPath = u8".\\D3D12\\"; }
 
 int main()
